e5ab298af9955a333d660b5aaf0f9ba726e83b30
Igor Zakharov <igorzakhar@gmail.com>
2022-06-08 18:30:41 +0300

    Add count_clicks function

--- url_shortener.py
+++ url_shortener.py
@@ -16,6 +16,17 @@ def shorten_link(token, url):
     return response.json()['id']
 
 
+def count_clicks(token, link):
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary'
+
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.get(api_url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()['total_clicks']
+
+
 def main():
     load_dotenv()
 
@@ -24,7 +35,9 @@ def main():
     long_url = input('Ссылка: ')
 
     try:
-        print('Битлинк', shorten_link(bitly_token, long_url))
+        short_url = shorten_link(bitly_token, long_url)
+        print('Битлинк', short_url)
+        print('Количество кликов:', count_clicks(bitly_token, short_url))
     except requests.exceptions.HTTPError as error:
         logging.exception(error, exc_info=False)
 
